7225f7e5fefdf95ef61b1ff3659cffb826a21dd4
echoyoung46 <vickyyoung0406@yeah.net>
2019-12-23 00:44:43 +0800

    add

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const Koa = require('koa');
+const {
+	ApolloServer,
+	gql
+} = require('apollo-server-koa');
+
+const books = [{
+		title: 'Harry Potter and the Chamber of Secrets',
+		author: 'J.K. Rowling',
+	},
+	{
+		title: 'Jurassic Park',
+		author: 'Michael Crichton',
+	},
+];
+
+const typeDefs = gql `
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+	Query: {
+		books: () => books,
+	},
+};
+
+const app = new Koa()
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+})
+
+server.applyMiddleware({
+	app
+})
+
+module.exports = {
+	run: async (port) => {
+		await app.listen(port)
+		console.log(`Server Listening ${port}`)
+	},
+	app
+}
